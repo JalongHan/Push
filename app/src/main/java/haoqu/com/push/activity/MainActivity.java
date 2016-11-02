9810f081655dfd9f0c2ffdf8a22107e8eec11d89
han <hjl999@126.com>
2016-11-02 17:05:48 +0800

    4

--- app/src/main/java/haoqu/com/push/activity/MainActivity.java
+++ app/src/main/java/haoqu/com/push/activity/MainActivity.java
@@ -5,28 +5,28 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.android.volley.VolleyError;
-
-import haoqu.com.push.Consts;
 import haoqu.com.push.R;
 import haoqu.com.push.service.HeartBeatService;
-import haoqu.com.push.tools.VolleyListenerInterface;
-import haoqu.com.push.tools.VolleyRequestTool;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+
+        initViews();
+
+
+
 
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(android.R.drawable.ic_dialog_email);
@@ -50,26 +50,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * 获取数据
+     * 初始化控件
      */
-    private void VolleyGetData() {
-        VolleyRequestTool.RequestPost(
-                MainActivity.this,
-                Consts.baseUrl + Consts.appIndex + Consts.weid,
-                Consts.appIndex,
-                null,
-                new VolleyListenerInterface(MainActivity.this, VolleyListenerInterface.mListener, VolleyListenerInterface.mErrorListener) {
-                    @Override
-                    public void onMySuccess(String result) {
-                        Log.i(TAG, "onMySuccess: ");
-                    }
-
-                    @Override
-                    public void onMyError(VolleyError error) {
-                        Log.i(TAG, "onMyError: ");
-                    }
-                });
+    private void initViews() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/haoqu/com/push/service/HeartBeatService.java
+++ app/src/main/java/haoqu/com/push/service/HeartBeatService.java
@@ -1,17 +1,27 @@
 package haoqu.com.push.service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
 import com.alibaba.fastjson.JSON;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import haoqu.com.push.Consts;
 import haoqu.com.push.JSONModel.ActionMessageBean;
+import haoqu.com.push.R;
+import haoqu.com.push.activity.MainActivity;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -24,6 +34,10 @@ import static android.content.ContentValues.TAG;
  */
 
 public class HeartBeatService extends Service{
+
+    private int id = 0;
+
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -37,27 +51,67 @@ public class HeartBeatService extends Service{
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public int onStartCommand(final Intent intent, int flags, int startId) {
 
-        OkHttpClient okHttpClient = new OkHttpClient();
-        final okhttp3.Request request = new okhttp3.Request.Builder()
-                .url(Consts.baseUrl + Consts.appIndex + Consts.weid)
-                .build();
-        Call call = okHttpClient.newCall(request);
-        call.enqueue(new Callback() {
+
+        Timer timer = new Timer(true);
+
+        timer.schedule(new TimerTask() {
             @Override
-            public void onFailure(Call call, IOException e) {
-                Log.i(TAG, "onFailure: ");
+            public void run() {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                final okhttp3.Request request = new okhttp3.Request.Builder()
+                        .url(Consts.baseUrl + Consts.appIndex + Consts.weid)
+                        .build();
+                Call call = okHttpClient.newCall(request);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        Log.i(TAG, "onFailure: ");
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+
+                        String result = response.body().string();
+                        Log.i("onResponse:", "onRespone: "+result);
+                        if (response.body() != null ) {
+                            ActionMessageBean actionMessageBean = JSON.parseObject(result,ActionMessageBean.class);
+
+                            //如果有通知内容,就解析发送通知栏通知.
+                            NotificationCompat.Builder mBuilder =
+                                    new NotificationCompat.Builder(HeartBeatService.this)
+                                            .setSmallIcon(R.mipmap.ic_launcher)
+                                            .setContentTitle("您有新消息").setNumber(++id)
+                                            .setDefaults(Notification.DEFAULT_VIBRATE)
+                                            .setAutoCancel(true)
+                                            .setContentText(actionMessageBean.getError());
+
+                            Intent resultIntent = new Intent(HeartBeatService.this, MainActivity.class);
+                            TaskStackBuilder stackBuilder = TaskStackBuilder.create(HeartBeatService.this);
+                            stackBuilder.addParentStack(MainActivity.class);
+                            stackBuilder.addNextIntent(resultIntent);
+                            PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+                            mBuilder.setContentIntent(resultPendingIntent);
+                            NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                            mNotificationManager.notify(0,mBuilder.build());
+
+
+
+                        }
+
+
+                    }
+                });
+
+
+
+
+
+
             }
+        },0,20*1000);
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                Log.i("onResponse:", "onRespone: "+response.body().string());
-
-                ActionMessageBean actionMessageBean = JSON.parseObject(response.body().string(),ActionMessageBean.class);
-
-            }
-        });
 
 
         return START_STICKY;
